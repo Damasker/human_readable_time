cca9baa2f64dedc785f744fb4b0761496677f8d1
Mykhayl <mykhayl@webcap.com>
2019-06-10 15:13:18 +0300

    fix path

--- lib/human_readable_time/view_hooks.rb
+++ lib/human_readable_time/view_hooks.rb
@@ -2,7 +2,7 @@ module LuxuryButtons
   module LuxuryButtons
     class Hooks  < Redmine::Hook::ViewListener
       def view_layouts_base_content(context)
-        javascript_include_tag(:application, :plugin => 'luxury_buttons')
+        javascript_include_tag(:application, :plugin => 'human_readable_time')
       end
     end
   end
